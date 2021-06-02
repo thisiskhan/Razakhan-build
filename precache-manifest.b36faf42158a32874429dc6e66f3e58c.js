0514dbb955187febaf1e988716f9d578da913947
Mohammed Raza Khan <rk6265766@gmail.com>
2021-06-02 21:06:11 +0530

    Raza Khan

---
+++ precache-manifest.b36faf42158a32874429dc6e66f3e58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16e3ab4d71ee30e828e82f526a636b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d2d075658e8bababb5",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "5f0e2868afea267ef46f",
+    "url": "/static/css/main.5582cb3d.chunk.css"
+  },
+  {
+    "revision": "22d2d075658e8bababb5",
+    "url": "/static/js/2.36b15cc3.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.36b15cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0e2868afea267ef46f",
+    "url": "/static/js/main.34b2fbcb.chunk.js"
+  },
+  {
+    "revision": "51b1263d09889fed3508",
+    "url": "/static/js/runtime-main.07ad49ca.js"
+  }
+]);
